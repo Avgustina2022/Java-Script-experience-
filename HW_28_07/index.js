eb9049ce6f51c3a5e1eb49a228e1b69e8113aaab
IRINA <108739078+Avgustina2022@users.noreply.github.com>
2022-07-28 20:58:07 +0300

    Add files via upload

---
+++ HW_28_07/index.js
@@ -0,0 +1,7 @@
+input.oninput = function () {
+  result.innerHTML = input.value;
+};
+document.querySelector(".big1").ondblclick = function () {
+  alert("SURPRISE");
+};
+console.log(document.querySelector(".big1"));
