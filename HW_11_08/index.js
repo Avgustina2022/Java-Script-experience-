8a813e093e2404263be1e81f5bfe2fa1ea69f4fc
IRINA <108739078+Avgustina2022@users.noreply.github.com>
2022-08-11 21:54:53 +0300

    Add files via upload

---
+++ HW_11_08/index.js
@@ -0,0 +1,24 @@
+console.log("Теперь запуск файлов быстрееэ");
+const employer = {
+  name: "Сергей Вальтер",
+  say() {
+    console.log("Hello");
+  },
+};
+console.log(employer);
+console.log(employer2.say());
+
+console.log(c);
+console.log(a);
+console.log(b);
+let arr = [1, 2, 3, 4, 5, [1, 1, 1], true, false];
+let obj = {
+  first: 1,
+  second: 2,
+  3: "third",
+  4: [1, 2, 3],
+  5: {
+    moscow: 777,
+    spb: 12345,
+  },
+};
